2ececfed1ad75569c1f90ea40c1004671832d34b
Marc Stan <marcstan@live.com>
2019-11-22 17:47:11 +0100

    added tools to ping and perf test website

---
+++ Frontdoor.Perf/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Frontdoor.Perf
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length != 2 || !int.TryParse(args[1], out int testDuration))
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("<url> <test duration in seconds>");
+                return;
+            }
+            var url = args[0];
+            using (var httpClient = new HttpClient())
+            {
+                Console.WriteLine("Ensuring site is warmed up..");
+                var endOfWarmup = DateTimeOffset.UtcNow.AddSeconds(5);
+                while (endOfWarmup > DateTimeOffset.UtcNow)
+                {
+                    // force warmup
+                    await httpClient.GetAsync(url);
+                }
+                Console.WriteLine("Preparing testrun..");
+                // verify endpoint is up and running
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                // begin perf test
+                Console.WriteLine($"Running test for {testDuration} seconds..");
+                var endOfTest = DateTimeOffset.UtcNow.AddSeconds(testDuration);
+                var watch = new Stopwatch();
+                var durations = new List<long>();
+                while (endOfTest > DateTimeOffset.UtcNow)
+                {
+                    watch.Restart();
+                    response = await httpClient.GetAsync(url);
+                    watch.Stop();
+                    response.EnsureSuccessStatusCode();
+                    durations.Add(watch.ElapsedMilliseconds);
+                }
+                Console.WriteLine("Test finished!");
+                var avg = durations.Average();
+                Console.WriteLine($"Avg: {avg:0.000}ms");
+            }
+        }
+    }
+}
---
+++ Frontdoor.Ping/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Frontdoor.Ping
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("<url>");
+                return;
+            }
+            var url = args[0];
+            using (var httpClient = new HttpClient())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var response = await httpClient.GetAsync(url);
+                        response.EnsureSuccessStatusCode();
+                        var content = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(content);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+        }
+    }
+}
